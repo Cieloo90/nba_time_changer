6101ee01230e4a0e662abb50dcd39a31b12a3bd9
Lucas Ciesielski <lukasz.ciesielski@currency-one.com>
2020-07-10 19:21:08 +0200

    better background.js

--- background.js
+++ background.js
@@ -1,19 +1,26 @@
+const wanted_url_regex = /nba.com/
+
+function is_wanted_site(tab) {
+  return tab.url.match(wanted_url_regex)
+}
+
+function change_icon(state) {
+  chrome.browserAction.setIcon({path: `images/${state}.png`});
+}
+
+function try_to_run_script(tab) {
+  let state = is_wanted_site(tab) ? 'active' : 'inactive'
+  change_icon(state)
+}
+
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-    if(tabs[0].url.match(/nba.com/)) {
-      chrome.browserAction.setIcon({path: `images/active.png`});
-    } else {
-      chrome.browserAction.setIcon({path: `images/inactive.png`});
-    };
+    try_to_run_script(tabs[0])
   });
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId) {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-    if(tabs[0].url.match(/nba.com/)) {
-      chrome.browserAction.setIcon({path: `images/active.png`});
-    } else {
-      chrome.browserAction.setIcon({path: `images/inactive.png`});
-    };
+    try_to_run_script(tabs[0])
   });
 });
